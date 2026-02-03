1fbbe540ebf41084f45f84f903582f5cdfc0ec08
Romee Katre <101556730+Romeekatre412@users.noreply.github.com>
2026-02-03 14:20:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-//your JS code here. If required.
+const form = document.getElementById("form");
+    const usernameInput = document.getElementById("username");
+    const passwordInput = document.getElementById("password");
+    const rememberCheckbox = document.getElementById("checkbox");
+    const existingBtn = document.getElementById("existing");
+
+    const savedUsername = localStorage.getItem("username");
+    const savedPassword = localStorage.getItem("password");
+
+    if (savedUsername && savedPassword) {
+      existingBtn.style.display = "block";
+    }
+
+    form.addEventListener("submit", function (e) {
+      e.preventDefault();
+
+      const username = usernameInput.value;
+      const password = passwordInput.value;
+
+      alert(`Logged in as ${username}`);
+
+      if (rememberCheckbox.checked) {
+        localStorage.setItem("username", username);
+        localStorage.setItem("password", password);
+        existingBtn.style.display = "block";
+      } else {
+        localStorage.removeItem("username");
+        localStorage.removeItem("password");
+        existingBtn.style.display = "none";
+      }
+    });
+
+    existingBtn.addEventListener("click", function () {
+      const savedUser = localStorage.getItem("username");
+      alert(`Logged in as ${savedUser}`);
+    });
